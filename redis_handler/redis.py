a30b1b67ab5eea258c204c291b6e5944111b6194
mobinkazemi <s.mobinkazemi@gmail.com>
2025-02-10 13:59:52 +0330

    read redis conf from file

--- redis_handler/redis.py
+++ redis_handler/redis.py
@@ -1,24 +1,27 @@
-from fastapi import FastAPI
+import configparser
 import redis
-import json
 
-app = FastAPI()
+config = configparser.ConfigParser()
 
+config.read("config.ini")
+host = config.get("redis", "REDIS_HOST")
+port = config.get("redis", "REDIS_PORT")
 # Initialize Redis connection
-redis_client = redis.Redis(host="192.168.17.139", port=6379, decode_responses=True)
+redis_client = redis.Redis(host=host, port=int(port), decode_responses=True)
+
 
 # print(json.loads(redis_client.get("GET_USER_INFO_KEY:09034214054")))
 def redis_set_value(key: str, value: str, ttl: int = 120):
     redis_client.set(key, str(value), ttl)
     return {"status": True}
 
+
 def redis_get_value(key: str):
     value = redis_client.get(key)
-    if(value == None):
+    if value == None:
         return {"status": False}
     return {"status": True, "value": value}
 
+
 def redis_del_value(key: str):
     redis_client.delete(key)
-
-
