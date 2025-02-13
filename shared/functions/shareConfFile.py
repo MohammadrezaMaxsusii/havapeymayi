1e77e138eac7dfcf2ab13492f4e6a728898a07c5
MohammadrezaMaxsusi <mohammadrezamaxsusi@gmail.com>
2025-02-13 12:06:57 +0330

    config file

--- shared/functions/shareConfFile.py
+++ shared/functions/shareConfFile.py
@@ -1,2 +1,7 @@
 import configparser
-def getConfigFile()
+config = configparser.ConfigParser()
+config.read("config.ini")
+def getConfigFile(section , part ):
+    return config.get(section, part)
+   
+    
